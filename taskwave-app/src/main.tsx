f30a7af27a6974ab5e79424fa174020626394349
leeseungyeol <211833@jnu.ac.kr>
2025-08-26 21:01:31 +0900

    logout

--- taskwave-app/src/main.tsx
+++ taskwave-app/src/main.tsx
@@ -1,11 +1,11 @@
 // src/main.tsx
+import './index.css'
+import App from './App.tsx'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx' // 1. AuthProvider 임포트
-import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
